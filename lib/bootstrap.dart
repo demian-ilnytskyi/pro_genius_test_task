4d54ec6d2839962d5ea85eba887956d6aa80044a
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-10-01 21:14:48 +0300

    did QA and add small improvments and fixing

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,11 +1,34 @@
 import 'dart:async' show FutureOr;
+import 'dart:developer';
 
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart' show Colors, Widget, runApp;
 import 'package:flutter/services.dart';
 import 'package:pro_genius_test_task/shared/helper/get_it_service_locator.dart';
 import 'package:pro_genius_test_task/shared/shared.dart';
 
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)', name: 'Bootstrap DATA');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log(
+      'Bloc Error Runtime Type - ${bloc.runtimeType}',
+      error: error,
+      stackTrace: stackTrace,
+      name: 'Bootstrap Bloc Error',
+    );
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  Bloc.observer = const AppBlocObserver();
   // Set only Vertical orientation
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
--- lib/shared/widget/modal_window/modal_window_widget.dart
+++ lib/shared/widget/modal_window/modal_window_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show SystemChannels;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pro_genius_test_task/l10n/l10n.dart';
@@ -54,7 +55,8 @@ class ModalWindowWidget extends StatelessWidget {
                     .read<ModalWindowBloc>()
                     .add(ModalWindowEvent.changeSize(details.delta.dy)),
                 onVerticalDragStart: (_) =>
-                    FocusScope.of(context).unfocus(), // hide keyboard
+                    // hide keyboard for WebView
+                    SystemChannels.textInput.invokeMethod('TextInput.hide'),
                 onVerticalDragEnd: (_) => context.read<ModalWindowBloc>().add(
                   const ModalWindowEvent.setHeighState(),
                 ),
@@ -77,6 +79,10 @@ class ModalWindowWidget extends StatelessWidget {
                               padding: const EdgeInsets.only(right: 16),
                               child: IconButton(
                                 onPressed: () {
+                                  // hide keyboard for WebView
+                                  SystemChannels.textInput.invokeMethod(
+                                    'TextInput.hide',
+                                  );
                                   final bloc = context.read<ModalWindowBloc>();
                                   if (bloc.state.showCloseIcon) {
                                     if (context.canPop()) {
@@ -110,13 +116,17 @@ class ModalWindowWidget extends StatelessWidget {
                   ),
                 ),
               ),
-              BlocSelector<ModalWindowBloc, ModalWindowState, double>(
-                selector: (state) => state.height,
-                builder: (context, height) => AnimatedContainer(
-                  duration: const Duration(milliseconds: 100),
-                  curve: Curves.easeOut,
-                  height: height,
-                  child: webView,
+              BlocBuilder<ModalWindowBloc, ModalWindowState>(
+                buildWhen: (previous, current) =>
+                    previous.height != current.height ||
+                    previous.maxWebViewHeight != current.maxWebViewHeight,
+                builder: (context, state) => Flexible(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeOut,
+                    height: state.height,
+                    child: webView,
+                  ),
                 ),
               ),
             ],
